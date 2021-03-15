b8cc8990b63e23021c8f2aedf681a6c8941899b4
Deltouille <34238909+Deltouille@users.noreply.github.com>
2021-03-15 14:22:50 +0100

    Modification Fichier SQL

--- Alexandre_Eval_SQL.sql
+++ Alexandre_Eval_SQL.sql
@@ -1,21 +1,21 @@
 Create Database EvaluationDB;
+
 USE EvaluationDB;
+
 Create Table T_Client(
 	Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     NomCli Varchar(30) NOT NULL,
     PrenomCli Varchar(30) NOT NULL,
-    AdresseCli Varchar(250),
     VilleCli Varchar(250),
     RueCli Varchar(250),
     CPCli INT(5) UNSIGNED,
     EmailCli Varchar(250) NOT NULL,
-    TelephoneCli Varchar(250) NOT NULL
+    TelephoneCli Varchar(10) NOT NULL
 );
 
 Create Table T_Cinema(
 	Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     NomCinema Varchar(30) NOT NULL,
-    AdresseCinema Varchar(250),
     VilleCinema Varchar(250),
     RueCinema Varchar(250),
     CPCinema INT(5) UNSIGNED,
@@ -29,4 +29,34 @@ Create Table T_Reservation(
     DateReservation Date NOT NULL,
     FOREIGN KEY(IdCli) REFERENCES T_Client(Id),
     FOREIGN KEY(IdCinema) REFERENCES T_Cinema(Id)
-)
+);
+/* Insertions */
+Insert Into T_Client(NomCli, PrenomCli, VilleCli, RueCli, CPCli, EmailCli, TelephoneCli)
+Values ("Jacque", "Pierre", "Rouen", "3 Rue des Potiers", 76000, "jacque.pierre@gmail.com", "0600000000");
+Insert Into T_Cinema(NomCinema, VilleCinema, RueCinema, CPCinema, NbPlaceCinema)
+Values ("Gaumont", "Rouen", "1 Rue du Gros Horloge", 76000, 500);
+Insert Into T_Reservation(IdCli, IdCinema, DateReservation)
+Values (1, 1, "2021-01-01");
+
+/* Selects */
+Select * From T_Client;
+Select * From T_Cinema;
+Select * From T_Reservation;
+
+/* Jointures Reservations */
+Select NomCli, NomCinema, DateReservation
+From T_Reservation as Reservation
+Inner Join T_Client as Client on Reservation.IdCli = Client.Id
+Inner Join T_Cinema as Cinema on Reservation.IdCinema = Cinema.Id;
+
+/* Creation d'utilisateur */
+Create User Administrateur;
+Create User Employé;
+/* Priviliges */
+Grant ALL privileges on evaluationdb To 'Administrateur';
+Grant select on T_Client To 'Employé';
+Grant select on T_Cinema To 'Employé';
+Grant select on T_Reservation To 'Employé';
+/* Drop Database */
+Drop Database EvaluationDB;
+
